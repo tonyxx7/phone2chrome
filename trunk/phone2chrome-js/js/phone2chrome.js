c524fce458ab84d1e1147d1c94abaea0f9ad0f53
luis.gvalle@gmail.com <luis.gvalle@gmail.com@10ef094f-f67e-1dd0-62c4-1025d278790b>
2011-04-17 17:43:24 +0000

    history functions

--- trunk/phone2chrome-js/js/phone2chrome.js
+++ trunk/phone2chrome-js/js/phone2chrome.js
@@ -23,12 +23,25 @@ phone2chrome.path = "phone2chrome"; //Path where 'pages' file should be.
 var a = "ENTER_YOURS";
 var b = "ENTER_YOURS";
 
+var pollIntervalMin = 1000 * 60;  // 1 minute
+var pollIntervalMax = 1000 * 60 * 60;  // 1 hour
+var requestFailureCount = 0;  // used for exponential backoff
+var requestTimeout = 1000 * 2;  // 5 seconds
+
 // First time
+// Opción para no abrir los ya visitados
 if (localStorage.getItem("deleteOld") == null) {
 	setDeleteOld(true);
 }
 
+// Opción para autoabrir enlaces a false por defecto
+if (localStorage["autoOpen"] == null) {
+	localStorage["autoOpen"] = false;
+}
+
 dropbox.init(a, b);
+//startRequest();
+
 
 // init
 function loadHandler() {
@@ -45,6 +58,8 @@ function loadHandler() {
 		// open pages in tabs
 		// FIXME: 404 error when file not found
 		$("#msg").html("No pending pages to open.");
+
+		// En realidad el callback es innecesario. La llamada a 'showPages' está implicita en el fichero pages.
 		dropbox.getFile(phone2chrome.path+"/pages", showPages.name);	  	
 	  	
 	} else {
@@ -57,42 +72,72 @@ function loadHandler() {
  
 }
 
-function deleted(){
-	console.log("deleted");
+function createTabs(pages) {
+	var tabs=new Array();
+	jQuery.each(pages, function(index, item){
+	       	// Old versions compatibility
+	       	if (item.date == null) {
+	       		var d = new Date();
+	       		item.date = d.getTime();
+	       	}
+	       	
+	       	if (getDeleteOld() && item.date <= localStorage[item.url]) {
+	       		//console.log("Already visited link: "+item.url);
+	       	} else {
+		       	//console.log("New visited link: "+item.url);
+		       	localStorage[item.url] = item.date;
+	       		if (isUrl(item.url)) {
+	       			chrome.tabs.create({url: item.url})		        
+	       		} else {
+	       			chrome.tabs.create({url: "textView.html"}, 
+	       				function (tab) {
+	       		    		chrome.tabs.sendRequest(tab.id, {title: item.title, body: item.url});
+	       			});
+	       		}
+		       	
+	       	}
+	});
+	//scheduleRequest();
 }
+
+function updateLinksCount(pages) {
+	var i=0;	
+	jQuery.each(pages, function(index, item){
+	       	// Old versions compatibility
+	       	if (item.date == null) {
+	       		var d = new Date();
+	       		item.date = d.getTime();
+	       	}
+	       	
+	       	if (getDeleteOld() && item.date <= localStorage[item.url]) {
+	       		//console.log("Already visited link: "+item.url);
+	       	} else {
+		       	i++;
+	       	}
+	});
+	if (i>0) {
+		chrome.browserAction.setBadgeText({text:String(i)});
+	} else {
+		chrome.browserAction.setBadgeText({text:""});
+	}
+}
+
 
 // Open pages in tabs
 function showPages(data) {
 	$("#msg").html("Opening tabs...");
 	var pages = data.pages;
-	var tabs=new Array();
-	jQuery.each(pages, function(index, item){
-	       	//console.log("Item "+index+" - "+item.title + ":" + item.url);
-	       	if (isUrl(item.url)) {
-		       	$("#items").append("<li>"+item.title+"</li>");
-		        chrome.tabs.create({url: item.url})	        
-		    } else {
-		    	console.log("Not valid url");
-		    	chrome.tabs.create({url: "textView.html"}, 
-		    		function (tab) {
-			    		chrome.tabs.sendRequest(tab.id, {title: item.title, body: item.url});
-				});
-		    }
-	});
+	var updateCount = localStorage["updateCount"];
 	
-	if (getDeleteOld()) {
-		dropbox.deleteItem(phone2chrome.path+"/pages", deleted.name);
-		// Old version Linux compatibility
-		//dropbox.deleteItem("Phone2Chrome/pages", null);	
-		
-		//
-		//var path = chrome.extension.getURL('pages');
-		//dropbox.uploadFile(phone2chrome.path, path, uploaded.name);
+	// Si estamos actualizando la lista de pendientes y no tenemos que abrirlos automáticamente
+	if ((updateCount == 1) && (localStorage["autoOpen"] == "false")) {
+		updateLinksCount(pages);
+	} else { // Sino estamos abriendo para crear nuevos tabs
+		createTabs(pages);
 	}
+	// Levanto el semaforo para permitir hacer peticiones
+	localStorage["updateCount"] = 0;
 }
-
-
-
 
 function isUrl(url) {
 	var regexp = /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
@@ -106,8 +151,9 @@ function doLogin() {
 	$("#info").html("Successfully linked with "+$("#email").val()+" Dropbox");
 	$("#loader").show();
 	
-	dropbox.login($("#email").val(), $("#password").val(), a, b);
-	
+	dropbox.login(escape($("#email").val()), escape($("#password").val()), a, b);
+	//console.log("ea");
+	scheduleRequest();
 }
 
 // Logout deletes accessToken and accessTokenSecret
@@ -127,3 +173,69 @@ function setDeleteOld(value) {
 function getDeleteOld() {
 	return localStorage["deleteOld"] == "true";
 }
+function setAuto(value) {
+	localStorage.setItem("autoOpen", value);
+	if (value == "true") {
+		setDeleteOld(true);
+		scheduleRequest();
+	} 
+}
+
+// Programa una petición "startRequest" para dentro de "delay" milisegundos
+// Se lanza la primera vez desde background.html
+function scheduleRequest() {
+	var delay = 5000; // 5 secs.
+	var randomness;
+	var exponent;
+		
+	if (localStorage["autoOpen"] == "false") {
+		randomness = Math.random() * 2;
+		exponent = 1;//Math.pow(2, requestFailureCount);
+		delay = Math.min(randomness * pollIntervalMin * exponent,
+		                     pollIntervalMax);
+		delay = Math.round(delay);		
+	}
+	
+ 	window.setTimeout(startRequest, delay);
+}
+
+// Hace una petición si no hay otra en curso. En cualquier caso programa la siguiente siempre
+function startRequest() {
+	// Si está levantado el semáforo no hago nuevas peticiones
+	if (localStorage["updateCount"] == 0) {
+		localStorage["updateCount"] = 1;
+		// Al lanzar la petición levanta el semáforo "updateCount"
+		dropbox.getFile(phone2chrome.path+"/pages", showPages.name);	
+	} 
+	scheduleRequest();
+}
+
+/** History functions **/
+function listAllLinksItems(){  
+    for (i=0; i<=localStorage.length-1; i++)  
+    {  
+        key = localStorage.key(i);  
+         
+        if (isUrl(key)) { 
+	        $("#historyList").append(
+	        	"<li>"+
+	        	"<a href='"+key+"'>"+key+"</a>"+
+	        	"</li>");
+        }
+    }  
+}  
+
+function clearAllLinksItems(){  
+    for (i=0; i<=localStorage.length-1; i++)  
+    {  
+        key = localStorage.key(i);  
+         
+        if (isUrl(key)) { 
+	        localStorage.removeItem(key);
+        }
+    } 
+	$("#historyList").html("");
+}  
+
+
+
